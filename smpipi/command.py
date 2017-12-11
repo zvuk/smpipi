c77d2ac60fb0a1163e919c0fab6d913c79c813d4
Rinat Khabibiev <srenskiy@gmail.com>
2017-12-11 17:13:13 +0300

    Revert "SMPP io-unaware proto"
    This reverts commit b843ccc56546cc03b2dd709fa277e3f12ac01e7a.

--- smpipi/command.py
+++ smpipi/command.py
@@ -16,7 +16,6 @@ def command_from_data(data):
 
 
 class Command(CommandMeta('CommandBase', (AttrDict,), {})):
-    is_response = False
     def __init__(self, **kwargs):
         self.update(kwargs)
         self['command_id'] = self.command_id
@@ -49,10 +48,6 @@ class Command(CommandMeta('CommandBase', (AttrDict,), {})):
         self['command_length'] = 16 + len(body)
         header = Header.encode(self)
         return header + body
-
-
-class CommandResp(Command):
-    is_response = True
 
 
 class Header(Packet):
@@ -104,39 +99,27 @@ class GenericNack(Command):
     command_id = 0x80000000
 
 
-class BindReceiverResp(CommandResp):
-    command_id = 0x80000001
-    body = BindResp
-
-
 class BindReceiver(Command):
-    response = BindReceiverResp
     command_id = 0x00000001
     body = Bind
 
 
-class BindTransmitterResp(CommandResp):
-    command_id = 0x80000002
+class BindReceiverResp(Command):
+    command_id = 0x80000001
     body = BindResp
 
 
 class BindTransmitter(Command):
-    response = BindTransmitterResp
     command_id = 0x00000002
     body = Bind
 
 
-class QuerySMResp(CommandResp):
-    command_id = 0x80000003
-    class body(Packet):
-        message_id = Field(NString(max=65))
-        final_date = Field(NString(max=17))
-        message_state = Field(int8)
-        error_code = Field(int8)
+class BindTransmitterResp(Command):
+    command_id = 0x80000002
+    body = BindResp
 
 
 class QuerySM(Command):
-    response = QuerySMResp
     command_id = 0x00000003
     class body(Packet):
         message_id = Field(NString(max=65))
@@ -145,43 +128,44 @@ class QuerySM(Command):
         source_addr = Field(NString(max=21))
 
 
-class SubmitSMResp(CommandResp):
-    command_id = 0x80000004
-    body = SubmitResp
+class QuerySMResp(Command):
+    command_id = 0x80000003
+    class body(Packet):
+        message_id = Field(NString(max=65))
+        final_date = Field(NString(max=17))
+        message_state = Field(int8)
+        error_code = Field(int8)
 
 
 class SubmitSM(Command):
-    response = SubmitSMResp
     command_id = 0x00000004
     body = Submit
 
 
-class DeliverSMResp(CommandResp):
-    command_id = 0x80000005
+class SubmitSMResp(Command):
+    command_id = 0x80000004
     body = SubmitResp
 
 
 class DeliverSM(Command):
-    response = DeliverSMResp
     command_id = 0x00000005
     body = Submit
 
 
-class UnbindResp(CommandResp):
-    command_id = 0x80000006
+class DeliverSMResp(Command):
+    command_id = 0x80000005
+    body = SubmitResp
 
 
 class Unbind(Command):
-    response = UnbindResp
     command_id = 0x00000006
 
 
-class ReplaceSMResp(CommandResp):
-    command_id = 0x80000007
+class UnbindResp(Command):
+    command_id = 0x80000006
 
 
 class ReplaceSM(Command):
-    response = ReplaceSMResp
     command_id = 0x00000007
     class body(Packet):
         message_id = Field(NString(max=65))
@@ -195,12 +179,11 @@ class ReplaceSM(Command):
         short_message = SizeField(Field(int8, 'sm_length'), String(max=254))
 
 
-class CancelSMResp(CommandResp):
-    command_id = 0x80000008
+class ReplaceSMResp(Command):
+    command_id = 0x80000007
 
 
 class CancelSM(Command):
-    response = CancelSMResp
     command_id = 0x00000008
     class body(Packet):
         service_type = Field(NString(max=6))
@@ -213,15 +196,18 @@ class CancelSM(Command):
         destination_addr = Field(NString(max=21))
 
 
-class BindTransceiverResp(CommandResp):
-    command_id = 0x80000009
-    body = BindResp
+class CancelSMResp(Command):
+    command_id = 0x80000008
 
 
 class BindTransceiver(Command):
-    response = BindTransceiverResp
     command_id = 0x00000009
     body = Bind
+
+
+class BindTransceiverResp(Command):
+    command_id = 0x80000009
+    body = BindResp
 
 
 class Outbind(Command):
@@ -231,32 +217,15 @@ class Outbind(Command):
         password = Field(NString(max=9))
 
 
-class EnquireLinkResp(CommandResp):
-    command_id = 0x80000015
-
-
 class EnquireLink(Command):
-    response = EnquireLinkResp
     command_id = 0x00000015
 
 
-class SubmitMultiResp(CommandResp):
-    command_id = 0x80000021
-
-    class body(Packet):
-        class UnsuccessDelivery(Packet):
-            dest_addr_ton = Field(int8)
-            dest_addr_npi = Field(int8)
-            destination_addr = Field(NString(max=21))
-            error_status_code = Field(int32)
-
-        message_id = Field(NString(max=65))
-        unsuccess_sme = SizeField(Field(int8, 'no_unsuccess'),
-                                  Array(UnsuccessDelivery))
+class EnquireLinkResp(Command):
+    command_id = 0x80000015
 
 
 class SubmitMulti(Command):
-    response = SubmitMultiResp
     command_id = 0x00000021
     class body(Packet):
         class DestAddr(Packet):
@@ -291,14 +260,24 @@ class SubmitMulti(Command):
         short_message = SizeField(Field(int8, 'sm_length'), String(max=254))
 
 
-class DataSMResp(CommandResp):
-    command_id = 0x80000103
-    body = SubmitResp
+class SubmitMultiResp(Command):
+    command_id = 0x80000021
+
+    class body(Packet):
+        class UnsuccessDelivery(Packet):
+            dest_addr_ton = Field(int8)
+            dest_addr_npi = Field(int8)
+            destination_addr = Field(NString(max=21))
+            error_status_code = Field(int32)
+
+        message_id = Field(NString(max=65))
+        unsuccess_sme = SizeField(Field(int8, 'no_unsuccess'),
+                                  Array(UnsuccessDelivery))
 
 
 class DataSM(Command):
-    response = DataSMResp
     command_id = 0x00000103
+
     class body(Packet):
         service_type = Field(NString(max=6))
         source_addr_ton = Field(int8)
@@ -310,3 +289,8 @@ class DataSM(Command):
         esm_class = Field(int8)
         registered_delivery = Field(int8)
         data_coding = Field(int8)
+
+
+class DataSMResp(Command):
+    command_id = 0x80000103
+    body = SubmitResp
